6dbf7f21dba39ea9d2c80ad3d92b742e29f61ca7
MiggleK <98611485+MiggleK@users.noreply.github.com>
2022-10-18 21:09:21 -0500

    Assignment2.2

--- Assignment2-Part2/Assignment2-Part2/Assignment2-Part2.cpp
+++ Assignment2-Part2/Assignment2-Part2/Assignment2-Part2.cpp
@@ -1,31 +1,80 @@
+/*
+* Miguel R. Kunkle
+* Created: 10/10/2022
+* 
+* Description: This program creates an abstract class, Employee, and derives 2 classes from it: Professional and NonProfessional
+* You can put in different aspects of the objects into a constructor passed down from the base class, and see information.
+* Note that since nonprofessionals don't get a salary or a set number of vacation days, these values are zeroed out.
+* 
+*/
 
 #include <iostream>
 #include "Employee.h"
 
-void NonProfessional::set_hours(double hoursWorked)
+double NonProfessional::weekly_salary()
 {
-    this->hoursWorked = hoursWorked;
+    double pay = hoursWorked * hourlyRate;
+
+    return pay;
 }
 
-void NonProfessional::set_rate(double hourlyRate)
+//this function is just used to calculate the vaction days earned **that week**, as per instructions.
+double NonProfessional::vacation_days()
 {
-    this->hourlyRate = hourlyRate;
+    double vacationRate = 0.0385;
+    double vacationHours = hoursWorked * vacationRate;
+
+    vacationDays = vacationHours / 8;
+
+    return vacationDays;
 }
 
-void Professional::set_salary(double salary)
+double NonProfessional::health_care()
 {
-    this->hoursWorked = hoursWorked;
+    double healthCareRate = 0.35;
+    double healthCareContributions = hoursWorked * healthCareRate;
+
+    return healthCareContributions;
 }
 
-void Professional::set_vacationDays(double vacationDays)
+double Professional::weekly_salary()
 {
-    this->hourlyRate = hourlyRate;
+    double weeklyPay = salary / 52; //52 weeks in a year to calculate weekly salary from yearly salary
+
+    return weeklyPay;
+}
+
+double Professional::vacation_days()
+{
+    return vacationDays; //This function need not do any calculations, as a professional employees vacations days are set, so its constructor does this functions job.
+}
+
+double Professional::health_care()
+{
+    double healthCareRate = 0.6;
+    double healthCareContributions = (salary / 52) * healthCareRate;
+
+    return healthCareContributions;
 }
 
 
 int main()
 {
+    //object creation
+    NonProfessional Timmy(0, 40, 20.50, 0);
+    Professional Billy(100000, 0, 0, 21);
 
+    //nonprofessional output
+    std::cout << "NonProfessional weekly pay: " << Timmy.weekly_salary() << std::endl;
+    std::cout << "NonProfessional vacation days: " << Timmy.vacation_days() << std::endl;
+    std::cout << "NonProfessional health care funds: " << Timmy.health_care() << std::endl;
+
+    std::cout << "-------------------------------" << std::endl;
+
+    //Professional output
+    std::cout << "Professional weekly pay: " << Billy.weekly_salary() << std::endl;
+    std::cout << "Professional vacation days: " << Billy.vacation_days() << std::endl;
+    std::cout << "Professional health care funds: " << Billy.health_care() << std::endl;
 
 }
 
--- Assignment2-Part2/Assignment2-Part2/Employee.h
+++ Assignment2-Part2/Assignment2-Part2/Employee.h
@@ -1,22 +1,7 @@
 #pragma once
 #ifndef EMPLOYEE_H_
 #define EMPLOYEE_H_
-/*
-* 
-* 
-* 
-* 
-* 
-* 
-* 
-* 
-* 
-* 
-* 
-* 
-* 
-* SETUP CONSTRUCTORSSSSSS
-*/
+
 class Employee
 {
 	protected:
@@ -25,31 +10,42 @@ class Employee
 		double hourlyRate;
 		double vacationDays;
 
-		//pure virtual functions
+		
 	public:
-		virtual double weekly_salary(double hoursWorked, double hourlyRate) const = 0;
-		virtual double health_care() const = 0;
-		virtual double vacation_days(double hoursWorked) const = 0;
+		//pure virtual functions
+		virtual double weekly_salary() = 0;
+		virtual double health_care() = 0;
+		virtual double vacation_days() = 0;
+
+		//constructor
+		Employee(double salary, double hoursWorked, double hourlyRate, double vacationDays)
+		{
+			this->hourlyRate = hourlyRate;
+			this->hoursWorked = hoursWorked;
+			this->salary = salary;
+			this->vacationDays = vacationDays;
+		}
+
+		//destructor
+		virtual ~Employee() {};
 };
 
 class Professional : public Employee
 {
-
 	public:
+		using Employee::Employee; //"Using" keyword is to allow the derived class to use the base class constructor
+
 		double weekly_salary();
 
 		double vacation_days();
 
 		double health_care();
-
-
 };
 
 class NonProfessional : public Employee
 {
 	public:
-		void set_hours(double hoursWorked);
-		void set_rate(double hourlyRate);
+		using Employee::Employee;
 
 		double get_hours() const
 		{
